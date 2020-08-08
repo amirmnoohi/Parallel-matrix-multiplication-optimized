ed80fcf3d4aebc0f2a11647f4ec821a3cf1c8f5a
AMIR MASOUD NOOHI <highlimner@gmail.com>
2020-08-08 11:37:41 -0700

    !

--- src/matrix.h
+++ src/matrix.h
@@ -14,7 +14,7 @@ typedef chrono::high_resolution_clock T;
 typedef chrono::microseconds Time;
 
 // Dimension of matrices
-#define DIM 1024
+#define DIM 2048
 
 // Maximum and Minimum value of random matrices
 #define MAX_VAL 100
